76c9a7214d4f5a9a8d9e7bc4502ebc2ca50fecee
JacekDobkiewicz <96839559+JacekDobkiewicz@users.noreply.github.com>
2023-10-10 19:28:11 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import "./style.css"
+import App from './App.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <StrictMode>
+    <App />
+  </StrictMode>
+  
+)
