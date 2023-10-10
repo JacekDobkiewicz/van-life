e40e4068833879d9d1eb24a7ddb55294c9938676
JacekDobkiewicz <96839559+JacekDobkiewicz@users.noreply.github.com>
2023-10-10 21:33:54 +0100

    added server, vans array and page, van tiles, styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
+import Vans from './pages/Vans'
+
+import "./server"
 
 export default function App() {
 
@@ -11,11 +14,13 @@ export default function App() {
       <Link className="site-logo" to="/">#VANLIFE</Link>
       <nav>
         <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
       </nav>
     </header>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About />} />
+      <Route path="/vans" element={<Vans />} />
     </Routes>
   </BrowserRouter>
   )
