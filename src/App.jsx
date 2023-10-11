372b6a05c477a971bbad7ba9f35a9cd1a31a9a9c
JacekDobkiewicz <96839559+JacekDobkiewicz@users.noreply.github.com>
2023-10-11 20:48:03 +0100

    fixed and added styling, added van detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
 
 import "./server"
 
@@ -21,6 +22,7 @@ export default function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About />} />
       <Route path="/vans" element={<Vans />} />
+      <Route path="/vans/:id" element={<VanDetail />} />
     </Routes>
   </BrowserRouter>
   )
